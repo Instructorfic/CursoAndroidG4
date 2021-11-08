3958667fed413e0b300d2649bcb158f25c02ca6d
Alexis Montaño <alexis.montano@info.uas.edu.mx>
2021-11-08 15:57:09 -0700

    Sesión 08-11-2021

--- app/src/main/java/com/fic/cursoandroid/MainActivity.java
+++ app/src/main/java/com/fic/cursoandroid/MainActivity.java
@@ -2,7 +2,9 @@ package com.fic.cursoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot_password);
+        setContentView(R.layout.activity_constraint_layout);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent showActivity = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(showActivity);
+            }
+        });
     }
 }
