2d2de6df1031b8f3a041a00fc6b7f9bfb91e273e
Alexis Montaño <alexis.montano@info.uas.edu.mx>
2021-12-08 15:59:54 -0700

    Sesión 08-12-2021

---
+++ app/src/main/java/com/fic/cursoandroid/capadatos/DataAccess.java
@@ -0,0 +1,29 @@
+package com.fic.cursoandroid.capadatos;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.fic.cursoandroid.utils.Constants;
+
+
+public class DataAccess extends SQLiteOpenHelper {
+
+
+    public DataAccess(Context context) {
+        super(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(String.format("CREATE TABLE %s (id integer primary key autoincrement, name text, paternal_surname text, maternal_surname text, email text, phone_number text, grade text, student_group text)", Constants.STUDENT_TABLE_NAME));
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
+
+
---
+++ app/src/main/java/com/fic/cursoandroid/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.fic.cursoandroid.utils;
+
+public class Constants {
+
+    public static final String DATABASE_NAME = "cursoandroid";
+    public static final String STUDENT_TABLE_NAME = "students";
+    public static final int DATABASE_VERSION = 1;
+}
